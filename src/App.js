c7ae5a54302d13c064b91237f7775066c416c706
Nazanin Delam <ndelam@netflix.com>
2020-08-13 22:36:38 -0700

    intialization

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from "react-dom";
+import MagicDropzone from "react-magic-dropzone";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import "@tensorflow/tfjs";
+
+class App extends React.Component {
+  state = {
+    model: null,
+    preview: "",
+    predictions: []
+  };
+
+  componentDidMount() {
+    mobilenet.load().then((model) => {
+      this.setState({
+        model: model
+      });
+    });
+  }
+
+  onDrop = (accepted, rejected, links) => {
+    this.setState({ preview: accepted[0].preview || links[0] });
+  };
+
+  onImageChange = (e) => {
+    this.state.model.classify(e.target).then((predictions) => {
+      this.setState({ predictions: predictions });
+    });
+  };
+
+  render() {
+    return (
+      <div className="Dropzone-page">
+        {this.state.model ? (
+          <MagicDropzone
+            className="Dropzone"
+            accept="image/jpeg, image/png, .jpg, .jpeg, .png"
+            multiple={false}
+            onDrop={this.onDrop}
+          >
+            <div className="Dropzone-content">
+              {this.state.preview ? (
+                <img
+                  alt="upload preview"
+                  onLoad={this.onImageChange}
+                  className="Dropzone-img"
+                  src={this.state.preview}
+                />
+              ) : (
+                "Choose or drop a file."
+              )}
+            </div>
+          </MagicDropzone>
+        ) : (
+          <div className="Dropzone">Loading model...</div>
+        )}
+        <div>
+          {this.state.predictions.map((item) => (
+            <div className="prediction" key={item.className}>
+              <div>{item.className}</div>
+              <div>{item.probability.toFixed(2)}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
