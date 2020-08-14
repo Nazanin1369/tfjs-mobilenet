7c0b369ac4673345fcbed8f26b00c753621011ed
Nazanin Delam <ndelam@netflix.com>
2020-08-13 23:56:32 -0700

    enhancements

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div id="particles-js">
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
