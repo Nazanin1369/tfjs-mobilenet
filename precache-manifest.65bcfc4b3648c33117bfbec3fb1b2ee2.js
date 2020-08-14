0d1ee8ba2e6fc24cd548414d52533c57844fc48f
Nazanin Delam <ndelam@netflix.com>
2020-08-14 00:17:57 -0700

    Updates

---
+++ precache-manifest.65bcfc4b3648c33117bfbec3fb1b2ee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578d6b73e79ab9c86e99f71210df71a7",
+    "url": "/tfjs-mobilenet/index.html"
+  },
+  {
+    "revision": "062ff28cee75c0b38ee6",
+    "url": "/tfjs-mobilenet/static/css/main.73291b27.chunk.css"
+  },
+  {
+    "revision": "5dfa82bd0960c1997f45",
+    "url": "/tfjs-mobilenet/static/js/2.162d890a.chunk.js"
+  },
+  {
+    "revision": "32d4aefaa40a2f347816b918e696a2f8",
+    "url": "/tfjs-mobilenet/static/js/2.162d890a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062ff28cee75c0b38ee6",
+    "url": "/tfjs-mobilenet/static/js/main.c35b77d4.chunk.js"
+  },
+  {
+    "revision": "ff2eb85b90edd711836d",
+    "url": "/tfjs-mobilenet/static/js/runtime-main.dcb574f8.js"
+  }
+]);
